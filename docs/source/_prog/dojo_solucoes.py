02c492f768e0cc719427e29b2cd769c24d0f7ff1
julia <juliabarbosa068@gmail.com>
2022-07-27 16:05:00 -0300

    adicionei as histórias dos problemas do dojo 1 e alterei a pagina de soluções

--- docs/source/_prog/dojo_solucoes.py
+++ docs/source/_prog/dojo_solucoes.py
@@ -155,6 +155,167 @@ assert maior_menor([10,3,5,6]) == 7
 assert maior_menor([7,2,10,9]) == 8
 assert maior_menor([2,10,7,2]) == 8
 
+_SETA_10 = {
+    "script_name": "dojo_10",    "script_div_id": "dojo_10",
+    "height": 200, "title": "Soma das matrizes"
+}  # _SEC_
+def soma_matriz(texto):
+    """ Retorna a soma dos números na matriz.
+    """
+
+
+
+assert soma_matriz([1,2,2,1]) == 6
+assert soma_matriz([1,1]) == 2
+assert soma_matriz([1,2,2,1,13]) == 6
+
+_SET11_ = {
+    "script_name": "dojo_11",    "script_div_id": "dojo_11",
+    "height": 200, "title": "Tem Dois 2"
+}  # _SEC_
+def tem22(texto):
+    """ Retorna TRue se a matriz tiver um 2 próximo
+     a um 2 em algum lugar.
+    """
+
+
+
+assert tem22([1,2,2]) == True
+assert tem22([1,2,1,2]) == False
+assert tem22([2,1,2]) == False
+
+_SET12_ = {
+    "script_name": "dojo_12", "script_div_id": "dojo_12",
+    "height": 200, "title": "Tijolos"
+}  # _SEC_
+def fazer_tijolos(pequeno, grande, objetivo):
+    """Retornar True se for possivel fazer a fileira de tijolos.
+    """
+
+
+_SET13_ = {
+    "script_name": "dojo_13",    "script_div_id": "dojo_13",
+    "height": 200, "title": "Soma dos valores"
+}  # _SEC_
+
+def soma_valores(a,b,c):
+    """Retorne a soma dos valores, mas se um dos valores for igual
+    a outro dos valores, ele não conta para a soma.
+    """
+
+
+assert soma_valores(1,2,3) == 6
+assert soma_valores(3,2,3) == 2
+assert soma_valores(3,3,3) == 0
+
+_SET14_ = {
+    "script_name": "dojo_14",    "script_div_id": "dojo_14",
+    "height": 200, "title": "numero repetido"
+}  # _SEC_
+def numero_sorte(numero):
+    """Retorna a soma de 3 valores, mas se um dos valores for 13, ele
+    não conta para a soma e os valores da direita não contam também.
+    """
+
+
+assert numero_sorte(1,2,3) == 6
+assert numero_sorte(1,2,13) == 3
+assert numero_sorte(1,13,3) == 1
+
+_SET15_ = {
+    "script_name": "dojo_15",    "script_div_id": "dojo_15",
+    "height": 200, "title": "Quilos de chocolate"
+}  # _SEC_
+def quilo_chocolate(matriz):
+    """Retorna o número de barras pequenas a serem usadas.
+    """
+
+
+
+assert quilo_chocolate(4,1,9) == 4
+assert quilo_chocolate(4,1,10) == -1
+assert quilo_chocolate(4,1,7) == 2
+_SET16_ = {
+    "script_name": "dojo_16",    "script_div_id": "dojo_16",
+    "height": 200, "title": "Letras repetidas"
+}  # _SEC_
+def letras_repetidas(palavra):
+    """Retornar a palavra com todas as letras repetidas
+    """
+
+
+
+assert letras_repetidas('The') == 'TThhee'
+assert letras_repetidas('AAbb') == 'AAAAbbbb'
+assert letras_repetidas('Ola Ara') == 'OOllaa AArraa'
+
+_SET17_ = {
+    "script_name": "dojo_17",    "script_div_id": "dojo_17",
+    "height": 200, "title": "Contando String"
+}  # _SEC_
+def contando_string(palavra):
+    ""
+
+
+assert contando_string('Code') == ('CCoCodCode')
+assert contando_string('abc') == ('aababc')
+assert contando_string('ab') == ('aab')
+
+_SET18_ = {
+    "script_name": "dojo_18",    "script_div_id": "dojo_18",
+    "height": 200, "title": "Gato e Cachorros"
+}  # _SEC_
+def gato_cachorro(palavra):
+  """Retorna True se aparecer 'gato' e 'cachorro' na string fornecida
+  """
+
+
+
+
+assert gato_cachorro('gatocachorro') == True
+assert gato_cachorro('gatogato') == False
+assert gato_cachorro('1gato1gacacachorro') == True
+_SET19_ = {
+    "script_name": "dojo_19",    "script_div_id": "dojo_19",
+    "height": 200, "title": "Final da palavra"
+}  # _SEC_
+def palavra_final(a,b):
+    """Retorna se apenas um é negativo ou se os dois são.
+    """
+
+
+assert palavra_final('Hiabc','abc') == True
+assert palavra_final('AbC', 'HiaBc') == True
+assert palavra_final('abc', 'abXabc') == True
+
+_SET20_ = {
+    "script_name": "dojo_20",    "script_div_id": "dojo_20",
+    "height": 200, "title": "Numeros inteiros"
+}  # _SEC_
+def matriz_inteiros(matriz):
+    """ 'Retorna o número de inteiros pares na matriz fornecida.
+    """
+
+
+assert matriz_inteiros([2,1,2,3,4]) == 3
+assert matriz_inteiros(2,2,0) == 3
+assert matriz_inteiros(1,3,5) == 0
+
+_SET9_ = {
+    "script_name": "dojo_9",    "script_div_id": "dojo_9",
+    "height": 200, "title": "Diferença maior e menor"
+}  # _SEC_
+def maior_menor(numeros):
+    """ Retorna a diferença entre o maior e o menor
+    valor na matriz.
+    """
+
+
+
+assert maior_menor([10,3,5,6]) == 7
+assert maior_menor([7,2,10,9]) == 8
+assert maior_menor([2,10,7,2]) == 8
+
 _SETA_ = {
     "script_name": "dojo_a",    "script_div_id": "dojo_a",
     "height": 200, "title": "Soma das matrizes"
--- docs/source/dojo_solucoes.rst
+++ docs/source/dojo_solucoes.rst
@@ -1,6 +1,148 @@
 .. include:: special.rst
 
-.. _modulo_esquenta_solucoesdojo_esquenta_2.pydojo_esquenta_2.py:
+.. _modulo_esquenta_solucoes.py:
+
+Problemas para o Dojo de Aquecimento 1
+======================================
+
+1. Multiplicando as palavras
+----------------------------
+
+Dada uma string e um int n não negativo, diremos que a frente da string são os primeiros 3 caracteres,
+ou o que estiver lá se a string for menor que 3. Retorna n cópias da frente;
+
+frente_vezes('Chocolate', 2) → 'ChoCho'
+frente_vezes('Chocolate', 3) → 'ChoChoCho'
+frente_vezes('Abc', 3) → 'AbcAbcAbc'
+
+.. raw:: html
+
+  <div id="dojo_0"></div>
+  <script type="text/python">
+      from ScriptWidget import ScriptBuilder
+      sw2 = ScriptBuilder("dojo_esquenta_1.py", height=200, title="dojo esquenta 0")
+  </script>
+
+
+2. Criando uma nova palavra
+--------------------------------------------------
+
+Dada uma string, retorne uma nova string feita de todos os outros caracteres começando com
+o primeiro, então "Hello" produz "Hlo".
+
+
+
+.. raw:: html
+
+  <div id="dojo_1"></div>
+
+3. Número Repetido
+---------------------------------
+
+Dado um array de ints, retorne a quantidade de números de 9 no array.
+
+.. raw:: html
+
+  <div id="dojo_2"></div>
+
+
+
+Dada uma string, retorne a contagem do número de vezes que uma substring
+de comprimento 2 aparece na string e também como os últimos 2 caracteres da
+string, então "hixxxhi" produz 1 (não contaremos a substring final).
+
+
+4. É ou não é
+------------------------------------------------------------------
+Dado um array de ints, retorne True se um dos primeiros 4 elementos do array for 9.
+O comprimento do array pode ser menor que 4.
+
+
+.. raw:: html
+
+  <div id="dojo_3"></div>
+
+5. Sequência de Números
+---------------------------------
+
+Dada uma matriz de ints, retorne True se a sequência de números 1, 2, 3 aparecer
+na matriz em algum lugar.
+
+.. raw:: html
+
+  <div id="dojo_4"></div>
+
+6. Explosão de Letras
+---------------------------------
+
+Dada uma string não vazia como "Code", retorne uma string como "CCoCodCode".
+
+.. raw:: html
+
+  <div id="dojo_5"></div>
+
+7. Posições Iguais
+---------------------------------
+
+Dadas 2 strings, a e b, retorne o número de posições onde elas contêm a mesma substring de comprimento 2.
+Portanto, "xxcaazz" e "xxbaaz" resultam em 3, já que as substrings "xx", "aa" e "az" aparecem no mesmo
+lugar em ambas as strings.
+
+.. raw:: html
+
+  <div id="dojo_6"></div>
+
+8. Negatividade
+---------------------------------
+
+Dados 2 valores int, retorne True se um for negativo e um for positivo.
+Exceto se o parâmetro "negativo" for True, então retorne True somente se ambos forem negativos.
+
+.. raw:: html
+
+  <div id="dojo_7"></div>
+
+9. Negação
+---------------------------------
+
+Dada uma string, retorne uma nova string onde "nega " foi adicionado à frente.
+No entanto, se a string já começar com "nega", retorne a string inalterada..
+
+.. raw:: html
+
+  <div id="dojo_8"></div>
+
+10. Eliminação
+---------------------------------
+
+Dada uma string não vazia e um int n, retorne uma nova string onde o char no índice n foi removido.
+O valor de n será um índice válido de um char na string original
+(ou seja, n estará no intervalo 0..len(str)-1 inclusive).
+
+.. raw:: html
+
+  <div id="dojo_9"></div>
+
+11. Trocação
+---------------------------------
+
+Dada uma string, retorne uma nova string onde o primeiro e a última letras foram trocadas.
+
+.. raw:: html
+
+  <div id="dojo_a"></div>
+
+12. Copiação
+---------------------------------
+
+Dada uma string, diremos que a frente são os 3 primeiros caracteres da string.
+Se o comprimento da string for menor que 3, a frente é o que estiver lá.
+Retorna uma nova string que tem 3 cópias da frente..
+
+.. raw:: html
+
+  <div id="dojo_b"></div>
+
 
 Problemas para o Dojo de Aquecimento 2
 =======================================
@@ -20,7 +162,7 @@ e pode ser feito sem nenhum loop.
   <div id="dojo_0"></div>
   <script type="text/python">
       from ScriptWidget import ScriptBuilder
-      sw2 = ScriptBuilder("dojo_esquenta_1.py", height=200, title="dojo esquenta 0")
+      sw2 = ScriptBuilder("dojo_esquenta_2.py", height=200, title="dojo esquenta 0")
   </script>
 
 
